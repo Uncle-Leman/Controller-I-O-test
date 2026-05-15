00a9c5d8973deeffe1d17349580421becb7a27a7
Uncle-Leman <muhammadaimanmaj@gmail.com>
2026-05-15 11:03:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,96 @@
+import time
+import json
+from machine import Pin
+from umqtt.simple import MQTTClient
 
+import config
+
+
+print("\n=== REMOTE APP START ===")
+
+
+# ======================
+# MQTT CONFIG
+# ======================
+DEVICE_ID = config.DEVICE_ID
+BASE_TOPIC = config.BASE_TOPIC
+TOPIC = BASE_TOPIC + "/" + DEVICE_ID
+
+MQTT_BROKER = config.MQTT_BROKER
+MQTT_PORT = config.MQTT_PORT
+MQTT_USERNAME = config.MQTT_USERNAME
+MQTT_PASSWORD = config.MQTT_PASSWORD
+
+
+# ======================
+# MQTT CONNECT
+# ======================
+def connect_mqtt():
+    client = MQTTClient(
+        client_id=DEVICE_ID,
+        server=MQTT_BROKER,
+        port=MQTT_PORT,
+        user=MQTT_USERNAME,
+        password=MQTT_PASSWORD,
+        keepalive=60
+    )
+
+    while True:
+        try:
+            print("Connecting MQTT...")
+            client.connect()
+            print("MQTT connected")
+            return client
+        except Exception as e:
+            print("MQTT retry:", e)
+            time.sleep(2)
+
+
+mqtt = connect_mqtt()
+
+
+# ======================
+# DIN
+# ======================
+pins = [
+    Pin(0, Pin.IN, Pin.PULL_UP),
+    Pin(1, Pin.IN, Pin.PULL_UP),
+    Pin(2, Pin.IN, Pin.PULL_UP),
+    Pin(3, Pin.IN, Pin.PULL_UP),
+]
+
+
+# ======================
+# MAIN LOOP
+# ======================
+last = 0
+
+while True:
+    now = time.ticks_ms()
+
+    if time.ticks_diff(now, last) > 2000:
+        data = {
+            "din0": bool(pins[0].value()),
+            "din1": bool(pins[1].value()),
+            "din2": bool(pins[2].value()),
+            "din3": bool(pins[3].value()),
+        }
+
+        payload = json.dumps(data)
+        print(payload)
+
+        try:
+            mqtt.publish(TOPIC, payload)
+        except Exception as e:
+            print("Publish error:", e)
+
+            try:
+                mqtt.disconnect()
+            except:
+                pass
+
+            mqtt = connect_mqtt()
+
+        last = now
+
+    time.sleep(0.1)
